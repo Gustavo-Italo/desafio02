d1e853b919e639d2c2e242ea5e67f5b5c5559a9f
Gustavo Italo <gussytalo@gmail.com>
2024-05-15 21:14:47 -0300

    desafio02

---
+++ script.js
@@ -0,0 +1,19 @@
+var nome = prompt('Digite o seu nome');
+
+var saudacao = nome;
+
+alert('Olá, ' + nome);
+
+var number1 = parseInt(prompt('Insira um number'));
+var number2 = parseInt(prompt('Insira outro number'));
+
+var resultado = number1 + number2
+
+alert ('A soma desses dois números é: ' + resultado);
+
+var texto1 = prompt('Digite um nome');
+var texto2 = prompt('Digite outro nome');
+
+var soma = texto1 + texto2
+
+alert ('A soma dessas duas palavras é: ' + soma);
